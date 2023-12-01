ccf537d7cc8c3dc2d86d40aef242d613558c584f
leandroarndt <poetalirico@gmail.com>
2023-12-01 20:41:56 -0300

    Comparable DB version

--- src/fichario/settings/db.py
+++ src/fichario/settings/db.py
@@ -8,4 +8,29 @@ class DBPath(abstract.Setting):
     _default = Path(Path.home(), 'fichario.sqlite3')
 
 class DBMigration(abstract.Setting):
-    _default = "0.0.1"
+    _version = 0
+    _subversion = 0
+    _revision = 0
+    
+    def __set__(self, obj, value):
+        self._version, self._subversion, self._revision = value.split('.')
+        self._version = int(self._version)
+        self._subversion = int(self._subversion)
+        self._revision = int(self._revision)
+        
+    def __get__(self, obj, objtype=None):
+        return f'{self._version}.{self._subversion}.{self._revision}'
+    
+    def __gt__(self, other):
+        if self._version > other._version:
+            return True
+        if self._subversion > other._subversion:
+            return True
+        if self._revision > other._revision:
+            return True
+        return False
+    
+    def __eq__(self, other):
+        if self._version == other._version and self._subversion == other._subversion and self._revision == other._revision:
+            return True
+        return False
