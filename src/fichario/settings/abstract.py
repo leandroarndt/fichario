d60a4acf53340811f6ccaa5b105ed9eb4bf2363f
leandroarndt <poetalirico@gmail.com>
2023-12-01 22:29:18 -0300

    Enhanced abstract classes
    Settings for syncing too.

--- src/fichario/settings/abstract.py
+++ src/fichario/settings/abstract.py
@@ -2,6 +2,7 @@ from abc import ABC
 from configparser import NoOptionError, NoSectionError
 
 class Setting(ABC):
+    _default = ''
     
     def __set_name__(self, owner, name):
         self._name = name
@@ -14,7 +15,57 @@ class Setting(ABC):
     
     def __set__(self, obj, value):
         try:
-            obj._parser.set(obj._attributes[self._name], self._name, value)
+            obj._parser.set(obj._attributes[self._name], self._name, str(value))
         except NoSectionError:
             obj._parser.add_section(obj._attributes[self._name])
-            obj._parser.set(self._owner._attributes[self._name], self._name, value)
+            obj._parser.set(self._owner._attributes[self._name], self._name, str(value))
+    
+    def __init__(self, default=None):
+        if default is not None:
+            self._default = default
+
+class BooleanSetting(Setting):
+    _default = False
+    
+    def __get__(self, obj, obj_type=None) -> bool:
+        if super().__get__(obj, obj_type) == 'True':
+            return True
+        return False
+
+class Version(Setting):
+    _version = 0
+    _subversion = 0
+    _revision = 0
+    
+    @staticmethod
+    def decode(value):
+        version, subversion, revision = value.split('.')
+        return int(version), int(subversion), int(revision)
+    
+    @staticmethod
+    def encode(value):
+        return f'{value[0]}.{value[1]}.{value[2]}'
+    
+    def __set__(self, obj, value):
+        if not isinstance(value, str):
+            value = self.encode(value)
+        self._version, self._subversion, self._revision = self.decode(value)
+        super().__set__(obj, value)
+    
+    def __gt__(self, other):
+        if self._version > other._version:
+            return True
+        if self._subversion > other._subversion:
+            return True
+        if self._revision > other._revision:
+            return True
+        return False
+    
+    def __eq__(self, other):
+        if self._version == other._version and self._subversion == other._subversion and self._revision == other._revision:
+            return True
+        return False
+    
+    def __init__(self, default='0.0.0', *args, **kwargs):
+        super(*args, **kwargs)
+        self._version, self._subversion, self._revision = self.decode(default)
--- src/fichario/settings/storage.py
+++ src/fichario/settings/storage.py
@@ -1,21 +1,35 @@
 from configparser import ConfigParser
 from pathlib import Path
-from . import db
+from . import abstract
 
 class AppSettings(object):
     _parser:ConfigParser = None
     _path = Path(Path.home(), 'fichario.ini')
-    db_path = db.DBPath()
-    db_migration = db.DBMigration()
+    db_path = abstract.Setting(Path(Path.home(), 'fichario.sqlite3'))
+    db_migration = abstract.Version()
+    use_dav = abstract.BooleanSetting()
+    dav_url = abstract.Setting()
+    dav_user = abstract.Setting()
+    dav_password = abstract.Setting()
+    use_google_drive = abstract.BooleanSetting()
+    sync_path = abstract.Setting(Path(Path.home(), 'fichario synchronization'))
     
     # Config sections
     _DB = 'DB'
-    _sections = (_DB,)
+    _REMOTE = 'REMOTE'
+    _SYNC = 'SYNC'
+    _sections = (_DB, _REMOTE, _SYNC)
     
     # Attribute->section mapping
     _attributes = {
         'db_path': _DB,
         'db_migration': _DB,
+        'use_dav': _REMOTE,
+        'dav_url': _REMOTE,
+        'dav_user': _REMOTE,
+        'dav_password': _REMOTE, # In Android, it should be FS encrypted
+        'use_google_drive': _REMOTE, # Google Drive credentials are stored in a separate file
+        'sync_path': _SYNC,
     }
     
     def save(self):
@@ -23,6 +37,7 @@ class AppSettings(object):
             self.__class__._parser.write(f)
     
     def __new__(cls):
+        modified = False
         if not cls._parser:
             cls._parser = ConfigParser()
             cls._path = Path(Path.home(), 'fichario.ini')
@@ -30,6 +45,8 @@ class AppSettings(object):
         for section in cls._sections:
             if not cls._parser.has_section(section):
                 cls._parser.add_section(section)
-        with open(cls._path, 'w') as f:
-            cls._parser.write(f)
+                modified = True
+        if modified:
+            with open(cls._path, 'w') as f:
+                cls._parser.write(f)
         return object.__new__(cls)
